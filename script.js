dfb01140bf8a23ec295c26969f150e28dd159d30
Kranlearn <christophesilvina@gmail.com>
2026-01-13 09:55:25 +0100

    script.js
    This file contains all the necessary functions for the game operation, including score display, email sharing, and input validation.

---
+++ script.js
@@ -0,0 +1,154 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les fonctions nécessaires au fonctionnement du jeu. 
+ * 
+ *********************************************************************************/
+
+/**
+ * @param {number} score : 
+ * @param {number} nbMotsProposes : 
+ */
+function afficherResultat(score, nbMotsProposes) {
+    // Récupération de la zone dans laquelle on va écrire le score
+    let spanScore = document.querySelector(".zoneScore span")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    // On place le texte à l'intérieur du span. 
+    spanScore.innerText = affichageScore
+}
+
+/**
+
+ * @param {string} proposition 
+ */
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition
+}
+
+/**
+ *  
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score. 
+ * @throws {Error}
+ */
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
+function validerNom(nom) {
+    if (nom.length < 2) {
+       throw new Error("Le nom est trop court. ") 
+    }
+   
+}
+/** 
+* @param {string} score : l' email. 
+ * @throws {Error}
+ */
+
+function validerEmail(email){
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+")
+    if (!emailRegExp.test(email)){
+        throw new Error(" L'Email n'est pas valide.")
+    }
+   
+}
+
+function afficherMessageErreur(message){
+     let spanErreurMessage = document.getElementById("erreurMessage")
+
+    if (!spanErreurMessage) {
+         let popup = document.querySelector(".popup")
+         spanErreurMessage = document.createElement("span")
+        spanErreurMessage.id = "erreurMessage"
+    
+     popup.append(spanErreurMessage)
+    }
+ spanErreurMessage.innerText = message
+
+}
+
+
+function gererFormulaire(scoreEmail){
+    try {
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+        validerNom(nom)
+        
+
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+        validerEmail(email)
+        afficherMessageErreur("")
+        afficherEmail(nom, email, scoreEmail)
+
+    } catch(erreur){
+        afficherMessageErreur(erreur.message)
+    }
+}
+
+function lancerJeu() {
+
+    initAddEventListenerPopup()
+    let score = 0
+    let i = 0
+    let listeProposition = listeMots
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+
+    afficherProposition(listeProposition[i])
+
+    var input = document.getElementById("myInput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+    // Gestion de l'événement click sur le bouton "valider"
+    btnValiderMot.addEventListener("click", () => {
+    })
+}
+
+        if (inputEcriture.value === listeProposition[i]) {
+            score++
+        }
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini")
+            btnValiderMot.disabled = true
+        } else {
+            afficherProposition(listeProposition[i])
+        }
+    })
+
+    // Gestion de l'événement change sur les boutons radios. 
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            
+
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                
+                listeProposition = listePhrases
+            }
+             
+            afficherProposition(listeProposition[i])
+        })
+    }
+
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault()
+            let scoreEmail = `${score}/ ${i}`
+            gererFormulaire(scoreEmail)
+    })
+
+
+    afficherResultat(score, i)
+}

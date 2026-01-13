7533ce025cf620367f10a5ac70241ab0081fbbf8
Kranlearn <christophesilvina@gmail.com>
2026-01-13 09:57:20 +0100

    main.js

---
+++ script/main.js
@@ -0,0 +1,18 @@
+
+lancerJeu()
+/*
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValideMot")
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zonePropositions")
+console.log(zoneProposition)
+
+let spanScore = document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionsource input")
+console.log(listeBtnRadio)
+*/
